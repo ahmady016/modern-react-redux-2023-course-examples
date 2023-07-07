8f22fdd2eb1fe3969ecc691f42f2de4e0a0812b3
ahmady023 <ahmady.e@gobussma.com>
2023-07-07 20:26:32 +0300

    init part-two react app with tailwindcss

---
+++ part-two/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="min-h-screen flex justify-center items-center bg-blue-400">
+			<h1 className="bg-blue-600 text-white text-3xl text-center font-bold p-3 rounded-lg">
+				Install & Setup Vite + React + Typescript + Tailwind CSS 3
+			</h1>
+		</div>
+	)
+}
+
+export default App
