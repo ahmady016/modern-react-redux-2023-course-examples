49f331898ec71e1597327c6d220ce18379fce4e8
ahmady016 <ahmady09@gmail.com>
2023-08-12 03:02:42 +0300

    Refactor Table to useSort hook 04

--- part-two/src/components/Table/SortableTable.tsx
+++ part-two/src/components/Table/SortableTable.tsx
@@ -3,6 +3,7 @@ import { FcCollapse, FcExpand } from 'react-icons/fc'
 
 import { TableProps } from './data'
 import Table from './Table'
+import useSort from './useSort'
 
 type SortIconProps = {
     currentField: string
@@ -20,47 +21,7 @@ const SortIcon : React.FC<SortIconProps> = ({ currentField, sortField, sortDirec
 }
 
 const SortableTable : React.FC<TableProps> = ({ data, columns, keyField, keyFn }) => {
-    const [sortDirection, setSortDirection] = React.useState<'asc' | 'desc' | undefined>(undefined)
-    const [sortField, setSortField] = React.useState<string | undefined>(undefined)
-    const [sortedData, setSortedData] = React.useState(data)
-
-    const handleSort = React.useCallback((e: React.MouseEvent<HTMLTableCellElement, MouseEvent>) => {
-        const currentField = e.currentTarget.id
-        // sort ascending by default when clicking on a header for the first time
-        if (sortDirection === undefined || sortField !== currentField) {
-            setSortDirection('asc')
-            setSortField(currentField)
-        }
-        // sort descending when clicking on an ascending sorted header
-        else if (sortDirection === 'asc') {
-            setSortDirection('desc')
-            setSortField(currentField)
-        }
-        // reset sorting when clicking on a descending sorted header
-        else if (sortDirection === 'desc') {
-            setSortDirection(undefined)
-            setSortField(undefined)
-        }
-    }, [sortField, sortDirection])
-
-    React.useEffect(() => {
-        if (sortDirection && sortField) {
-            let newSortedData = [...data].sort((a, b) => {
-                const valueA = a[sortField]
-                const valueB = b[sortField]
-                const sortOrder = sortDirection === 'asc' ? 1 : -1
-                if (typeof valueA === 'string' && typeof valueB === 'string')
-                    return valueA.localeCompare(valueB) * sortOrder
-                if (typeof valueA === 'number' && typeof valueB === 'number')
-                    return (valueA - valueB) * sortOrder
-                throw Error(`Cannot compare values of type ${typeof valueA}`)
-            })
-            setSortedData(newSortedData)
-        }
-        console.log("🚀: SortableTable -> handleSort -> sortField:", sortField)
-        console.log("🚀: SortableTable -> handleSort -> sortDirection:", sortDirection)
-    }, [sortField, sortDirection])
-
+    const { sortDirection, sortField, sortedData, handleSort } = useSort(data)
     const sortableColumns = columns.map(column =>
         (column.sortable)
             ?   {   ...column,
