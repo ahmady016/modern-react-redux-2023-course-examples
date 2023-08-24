bbea7f1b905442f5073dd5dc14b51d0a9a20e2c9
ahmady016 <ahmady09@gmail.com>
2023-08-24 13:57:44 +0300

    handle reset authors, books state

--- part-three/src/features/Home/HomePage.tsx
+++ part-three/src/features/Home/HomePage.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
+import { useAppDispatch } from '../../reduxStore'
+import { resetBooksAndAuthors } from '../reduxActions'
+
+import Button from '../../components/Button'
 import Authors from './Authors'
 import Books from './Books'
-import Button from '../../components/Button'
 
 const HomePage: React.FC = () => {
+    const dispatch = useAppDispatch()
+    const handleReset = React.useCallback(() => void dispatch(resetBooksAndAuthors()), [dispatch])
+
 	return (
         <div className="w-full p-4">
-            <div className="flex justify-center items-center">
-                <Button className="w-1/6" danger rounded>Reset All</Button>
+            <div className="w-1/6 m-auto">
+                <Button className="w-1/6" danger rounded onClick={handleReset}>Reset All</Button>
             </div>
             <hr className="h-px my-8 bg-gray-300 border-0" />
             <Authors />
