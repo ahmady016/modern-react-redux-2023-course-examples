d5a3ba3adb565873ee599bfdd12f1c94f3eacdc9
ahmady016 <ahmady09@gmail.com>
2023-09-17 13:10:30 +0300

    Build CourseInfoCard

--- part-three/src/features/Courses/Details/CoursePage.tsx
+++ part-three/src/features/Courses/Details/CoursePage.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+import CourseInfoCard from './CourseInfoCard'
 import SectionForm from './SectionForm'
 import LessonForm from './LessonForm'
 import CourseDetailsView from './CourseDetailsView'
@@ -6,11 +8,12 @@ import CourseDetailsView from './CourseDetailsView'
 const CoursePage: React.FC = () => {
 	return (
 		<div>
-			<h2 className="text-center text-3xl font-bold">Course Details</h2>
-			<hr className="h-px my-8 bg-gray-300 border-0" />
-			<SectionForm />
-			<hr className="h-px my-8 bg-gray-300 border-0" />
-			<LessonForm />
+			<CourseInfoCard />
+			<hr className="h-px mb-8 bg-gray-300 border-0" />
+			<div className="grid gap-3 grid-cols-2">
+				<SectionForm />
+				<LessonForm />
+			</div>
 			<hr className="h-px my-8 bg-gray-300 border-0" />
 			<CourseDetailsView />
 		</div>
